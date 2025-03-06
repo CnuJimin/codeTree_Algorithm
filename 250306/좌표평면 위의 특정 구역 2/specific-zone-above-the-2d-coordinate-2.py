88523899963d500fa2eb5c641a19f591d779321b
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-06 16:06:28 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250306/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+x = []
+y = []
+
+for _ in range(N):
+    a ,b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+
+answer = 0 
+#모든 N에 대해 i번째 좌표를 제외했을 때 -> N
+for i in range(N):
+    #가장 큰 x,y와 가장 작은 x,y를 구한다 -> 4N => o(n^2) 
+    n_x = x[:i] + x[i+1:]
+    n_y = y[:i] + y[i+1:]
+    max_x = max(n_x)
+    min_x = min(n_x)
+    max_y = max(n_y)
+    min_y = min(n_y)
+    area = (max_x - min_x) * (max_y - min_y)
+
+    answer = min(answer, area)
+
+print(area)
