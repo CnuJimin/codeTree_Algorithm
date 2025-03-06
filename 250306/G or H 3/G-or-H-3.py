b1e756d72000e009d7b09eea5402f9c7b6e5762c
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-06 15:50:22 +0900

    Title: G or H 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250306/G or H 3/G-or-H-3.py
+++ 250306/G or H 3/G-or-H-3.py
@@ -1,18 +1,25 @@
 n, k = map(int, input().split())
-arr = [list(map(str, input().split()))for i in range(n)]
+pos = []
+alp = []
+
+for i in range(n):
+    a, b = input().split()
+    pos.append(int(a))
+    alp.append(b)
+
 
 answer = 0 
 
 # 모든 위치 i, i + k에 대해서 
-for i in range(0, 1001):
+for i in range(0, 1000-k+1):
     score = 0
     start = i
     end = i + k
     #힐거 한다, N을 돌면서 해당 구역에 점수가 몇점인지 구한다. 
 
     for j in range(n):
-        if start<=int(arr[j][0])<=end : 
-            if arr[j][1] == 'G':
+        if start<= pos[j] <=end : 
+            if alp[j] == 'G':
                 score += 1
             else:
                 score += 2
