73ecb24d09f98b84a47791d5a0125f29fc112b5e
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-07 09:18:00 +0900

    Title: 바구니 안의 사탕 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250307/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250307/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,16 +1,27 @@
 n, k = map(int, input().split())
 candy = [tuple(map(int, input().split())) for i in range(n)] #[사탕의 개수, 바구니 위치]
 
-answer = 0
-# 모든 경우에 대해 / 모든 c위치에 대해서 
-for c in range(101):
-    # [c-k, c+k]구간 사이에 사탕의 개수를 찾고, 최댓값을 갱신한다
-    st = c - k if c - k >= 0 else 0
-    end = c + k
-    cnt = 0 
-    for i in range(n):
-        if st<= candy[i][1] <= end :
-            cnt += candy[i][0]
-        answer = max(answer, cnt)
+# answer = 0
+# # 모든 경우에 대해 / 모든 c위치에 대해서 
+# for c in range(101):
+#     # [c-k, c+k]구간 사이에 사탕의 개수를 찾고, 최댓값을 갱신한다
+#     st = c - k if c - k >= 0 else 0
+#     end = c + k
+#     cnt = 0 
+#     for i in range(n):
+#         if st<= candy[i][1] <= end :
+#             cnt += candy[i][0]
+#         answer = max(answer, cnt)
+
+# print(answer)
+
+arr = [0] * 101
+answer = -1 
+
+for i in range(n):
+    arr[candy[i][1]] = candy[i][0]
+
+for i in range(101):
+    answer = max(answer, sum(arr[i - k : i + k + 1]))
 
 print(answer)
