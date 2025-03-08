12c5239c8d82778b463deb7c73255cb8607b018b
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-08 20:02:08 +0900

    Title: 이상한 폭탄 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250308/이상한 폭탄 2/strange-bomb-2.py
+++ 250308/이상한 폭탄 2/strange-bomb-2.py
@@ -5,7 +5,7 @@ arr = [int(input()) for _ in range(n)]
 ans = -1
 
 #각 폭발 i에 대해
-for i in range(n-k):
+for i in range(n-k + 1):
     search = arr[i:i+k+1]
     #k범위 안에 동일한 폭탄이 존재하는지 확인 
     if len(search) != len(set(search)):
