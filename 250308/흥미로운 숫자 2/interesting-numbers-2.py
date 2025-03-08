d45917dd282e4c9314175630db0c08578370aae1
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-08 21:33:49 +0900

    Title: 흥미로운 숫자 2, Time: 184ms, Memory: 18MB, Status: Passed - CodeTree

--- 250308/흥미로운 숫자 2/interesting-numbers-2.py
+++ 250308/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,10 +1,24 @@
 x ,y = map(int, input().split())
 #x,y 사이의 모든 숫자에 대해 
-cnt = 0
+ans = 0
 for i in range(x, y+1):
+    cnt = 0
     num = list(str(i))
-    if len(num) - len(set(num)) == 1:
-        cnt += 1
+    digits = [0] * 10
+    digits_num = len(num)
 
-print(cnt)
+    for j in num:
+        digits[int(j)] += 1
+    
+    interesing = False
+
+    for j in range(10):
+        if digits[j] == digits_num -1 : 
+            interesing = True
+    
+    if interesing:
+        ans += 1
+
+
+print(ans)
 #숫자가 2개 이상 다르면 숫자를 센다 
