afc9cbebcc897e9740124b27248cdaa965f90916
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-08 19:19:44 +0900

    Title: 스승의 은혜 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250308/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,25 @@
+n, b = map(int, input().split())
+
+arr = [int(input()) for _ in range(n)]
+
+ans = 0
+
+for i in range(n):
+    total = 0
+    cnt = 0
+    for j in range(n):
+        if j == i :
+            if (total + arr[j]//2) <= b:
+                total += arr[j] // 2
+                cnt += 1
+                continue
+
+        if (total + arr[j]) <= b:
+            total += arr[j]
+            cnt += 1
+        else:
+            break
+    
+    ans = max(cnt, ans)
+
+print(ans)
