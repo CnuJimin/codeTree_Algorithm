dda1a0a3c42d794b55776f601f2e58601d63b5c7
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-09 17:57:31 +0900

    Title: 2개 이상의 알파벳, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250309/2개 이상의 알파벳/more-than-one-alphabet.py
+++ 250309/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -14,7 +14,7 @@ word = input()
 def up(word):
     cnt = 0
     for i in range(len(word) -1 ):
-        if word[i] != word[i+1]:
+        if word[-1] != word[i+1]:
             cnt += 1
     
     if cnt >= 2:
