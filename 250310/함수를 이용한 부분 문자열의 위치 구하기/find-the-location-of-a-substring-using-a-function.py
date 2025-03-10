e00d6dd24ee0c8223d24af7ce6f16b211f61411e
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-10 13:35:51 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 250310/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
+++ 250310/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.py
@@ -1,9 +1,30 @@
 word = input()
 part = input()
 
-def contains(word, part):
-    if part in word:
-        return word.index(part)
+
+
+
+def is_substr(start_idx):
+    n, m = len(word), len(part)
+
+    if start_idx + m - 1 >= n :
+        return False
+    
+    for i in range(m):
+        if word[i + start_idx] != part[i]:
+            return False
+
+    return True    
+    
+
+
+def get_index():
+    n = len(word)
+    for i in range(n):
+        if is_substr(i):
+            return i 
+            
     return -1 
 
-print(contains(word, part))
+
+print(get_index())
