a1c7f353a3322e0ce99d2e742127db4a02f9cd71
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-12 20:35:31 +0900

    Title: 벌금은 누구에게, Time: 64ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250312/벌금은 누구에게/who-will-pay.py
+++ 250312/벌금은 누구에게/who-will-pay.py
@@ -1,1 +1,25 @@
 n, m, k = map(int, input().split())
+
+students = [0] * n 
+
+find = False
+
+answer = -1 
+
+for i in range(m):
+    who = int(input())
+
+    students[who - 1] += 1
+
+    for j in range(n):
+        if students[j] >= k :
+            find = True
+            answer = j + 1
+            break
+
+    if find:
+        print(answer)
+        break
+
+
+    
