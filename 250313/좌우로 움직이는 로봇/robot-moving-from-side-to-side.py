63ab131c0b4fb4e1d4e159ce936ec31b910e400e
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-13 16:23:56 +0900

    Title: 좌우로 움직이는 로봇, Time: 208ms, Memory: 52MB, Status: Passed - CodeTree

--- 250313/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250313/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -4,7 +4,7 @@ a_arr = [0] * 2000010
 b_arr = [0] * 2000010
 
 #A의 이동 
-time = 0
+a_time = 0
 
 for _ in range(n):
     dist, way = map(str, input().split())
@@ -12,18 +12,18 @@ for _ in range(n):
 
     if way == "L":
         for i in range(dist):
-            time += 1
-            a_arr[time] = a_arr[time - 1] - 1
+            a_time += 1
+            a_arr[a_time] = a_arr[a_time - 1] - 1
     else:
         for i in range(dist):
-            time += 1
-            a_arr[time] = a_arr[time - 1] + 1
+            a_time += 1
+            a_arr[a_time] = a_arr[a_time - 1] + 1
     
     # print(a_arr[time])
 
 
 #B의 이동 
-time = 0 
+b_time = 0 
 
 for _ in range(m):
     dist, way = map(str, input().split())
@@ -31,22 +31,51 @@ for _ in range(m):
 
     if way == "L":
         for i in range(dist):
-            time += 1
+            b_time += 1
 
-            b_arr[time] = b_arr[time - 1] - 1
+            b_arr[b_time] = b_arr[b_time - 1] - 1
     else:
         for i in range(dist):
-            time += 1
+            b_time += 1
 
-            b_arr[time] = b_arr[time - 1] + 1
+            b_arr[b_time] = b_arr[b_time - 1] + 1
     # print(b_arr[time])
+
+if a_time < b_time:
+    for i in range(a_time + 1, b_time + 1):
+        if a_arr[i] == 0:
+            a_arr[i] = a_arr[a_time]
+else:
+    for i in range(b_time + 1, a_time + 1):
+        if b_arr[i] == 0:
+            b_arr[i] = b_arr[b_time]
+
+
+
+
+max_time = max(a_time, b_time)
+
+
+
+
 
 
 answer = 0 
 
 
-for i in range(1, time + 1):
+# for i in range(1, time + 1):
+#     print(a_arr[i], end = " ")
+
+# print()
+# for i in range(time + 1):
+#     print(b_arr[i], end = " ")
+
+
+
+
+for i in range(1, max_time):
     if a_arr[i] != b_arr[i] and a_arr[i+1] == b_arr[i+1]:
+        # print(i)
         answer += 1 
 
 print(answer)
