07b52ee25a7ff3a2707ff6f1c06811051d7f2197
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-14 15:58:06 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 93ms, Memory: 19MB, Status: Runtime Error - CodeTree

---
+++ 250314/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -0,0 +1,63 @@
+n, m = map(int, input().split())
+
+#배열 만들어주기 
+graph = [list(map(int, input().split())) for _ in range(m)]
+
+#방문 여부 배열 만들어주기 
+visited = [[0 for _ in range(n)] for _ in range(m)]
+
+order = 1
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def can_go(x,y):
+    return in_range(x,y) and graph[x][y] != 0 and visited[x][y] == 0 
+
+#dfs 함수 만들기 
+def dfs(x, y):
+    global order
+
+    graph[x][y] = order
+
+    dx = [1, 0]
+    dy = [0, 1]
+
+    for i in range(2):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if can_go(nx,ny):
+            visited[nx][ny] = 1
+            order += 1
+            dfs(nx,ny)
+
+
+
+visited[0][0] = 1
+
+dfs(0,0)
+
+
+# for i in range(n):
+#     for j in range(n):
+#         print(graph[i][j], end = " ")
+#     print()
+
+# print("**********")
+
+# for i in range(n):
+#     for j in range(n):
+#         print(visited[i][j], end = " ")
+#     print()
+
+
+if graph[n-1][n-1] == 1 :
+    print("0")
+else:
+    print("1")
+
+
+
+
+
