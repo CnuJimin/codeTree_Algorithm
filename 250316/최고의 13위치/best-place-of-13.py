d8d082cded2a9a355e80b09776b32fae02b09d51
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-16 21:11:35 +0900

    Title: 최고의 13위치, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250316/최고의 13위치/best-place-of-13.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+arr = [list(map(int, input().split()))for _ in range(n)]
+
+ans = 0 
+
+for i in range(n):
+    for j in range(n-2):
+        area = arr[i][j] + arr[i][j+1] + arr[i][j+2]
+        ans = max(ans, area)
+
+print(ans)
