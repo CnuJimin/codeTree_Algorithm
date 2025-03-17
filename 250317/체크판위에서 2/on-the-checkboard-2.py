b664c82a8a06e426a131d128e32a9d7bf1c71f53
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-17 15:56:57 +0900

    Title: 체크판위에서 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250317/체크판위에서 2/on-the-checkboard-2.py
+++ 250317/체크판위에서 2/on-the-checkboard-2.py
@@ -2,13 +2,15 @@
 
 r,c = map(int, input().split())
 
-arr = [list(map(str, input().split()))for i in range(c)]
+arr = [list(map(str, input().split()))for i in range(r)]
 
 cnt = 0 
 
-for x in range(1, r - 1):
-    for y in range(1, c - 1):
-        if arr[x][y] == "B":
-            cnt += (r - x - 2) * (c - y - 2)
+for i in range(1, r - 1):
+    for j in range(1, c - 1):
+        for k in range(i+1, r - 1):
+            for l in range(j+1, c-1):
+                if arr[0][0] != arr[i][j] and arr[k][l] != arr[r-1][c-1]:
+                    cnt += 1
     
 print(cnt)
