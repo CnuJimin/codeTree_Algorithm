1c02acf122bcd0c29b9d0433aa7b3d8a70e58692
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-19 14:26:20 +0900

    Title: 겹치지 않는 선분 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250319/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 250319/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -2,30 +2,28 @@
 # 선분을 정렬하고, 자기보다 앞에 있는 선분들의 x2 값의 최댓값 기준으로 그것보다 앞에 있으면 겹치는 선분 
 
 n = int(input())
+x = [] 
+y = [] 
 
-lines = [tuple(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    a, b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+# lines = [tuple(map(int, input().split())) for _ in range(n)]
 
-lines.sort(key = lambda x : x[0])
+ans = 0
 
-max_y = lines[0][1]
-
-# print(*lines)
-
-ans = 1 
-for i in range(1, n):
-    x,y = lines[i]
-
-    if y <= max_y:
+for i in range(n):
+    overlap = False
+    for j in range(n):
+        if i == j : 
+            continue 
+        
+        if (x[i] <= x[j] and y[i] >= y[j]) or (x[i] >= x[j] and y[i] <=  y[j]) :
+            overlap = True 
+            break
+        
+    if overlap :
         ans += 1
-        # print("max_y = ", max_y)
-        # print("x2 = ", y)
 
-
-
-    if y > max_y:
-        max_y = y 
-    
-
-
-
-print(n - ans)
+print(ans)
