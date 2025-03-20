46fca30ad5171cc0598abf2ae4dddf0dc780bef0
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-20 16:44:22 +0900

    Title: 상해버린 치즈, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250320/상해버린 치즈/rotten-cheese.py
+++ 250320/상해버린 치즈/rotten-cheese.py
@@ -1,85 +1,63 @@
-# n, m, d, s = map(int, input().split())
-
-# arr = [[0 for _ in range(51)]for _ in range(51)]
-
-# eat = [list(map(int, input().split())) for _ in range(d)] # [몇번쨰 사람, 몇번째 치즈, 시간]
-
-# sick = [list(map(int, input().split())) for _ in range(s)] # [몇번째 사람, 언제아팠는지]
-
-# spoil_chz = [0] * 51 
-
-# #t초까지 반복하면서, 치즈를 배열에 기록함, 배열은 51*51으로 만들어서, 각 사람이 몇번쨰 치즈를 먹었는지 기록 
-# for t in range(1, 101):
-#     #t시간에 아팠다면, 그 아픈 사람이 t시간 전까지 어떤 치즈를 먹었는지 기록 
-#     for i in range(s):
-#         if sick[i][1] == t:
-#             for j in range(51):
-#                 if arr[sick[i][0]][j] == 1 :
-#                     # print(t)
-#                     # print(*arr[sick[i][0]])
-#                     spoil_chz[j] += 1 
-
-
-#     #누가 치즈를 먹었는지 기록 
-#     for i in range(d):
-#         if eat[i][2] == t:
-#             arr[eat[i][0]][eat[i][1]] = 1 
-#             # print(*arr[eat[i][0]])
-
-# #만약 t초에 어떤 사람이 아팠다면, 그 전에 그 사람이 먹었던 치즈는 모두 상했다고 판단, 그리고 마지막에 해당 치즈를 먹은 인원은 전부 약을 먹어야 하는 인원이라고 생각 
-
-# # spoil_chz = list(set(spoil_chz))
-# # print(spoil_chz)
-# chz = []
-# for i in range(51):
-#     if spoil_chz[i] == s :
-#         chz.append(i)
-
-
-# ans = 0 
-
-# # print(*chz)
-
-# for i in range(51):
-#     for j in chz:
-#         if arr[i][j] == 1 :
-#             ans +=1 
-#             break
-
-# print(ans)
-
-
-
 n, m, d, s = map(int, input().split())
 
-arr = [[0 for _ in range(51)] for _ in range(51)]
+class Info_1:
+    def __init__(self, p, m, t):
+        self.p = p 
+        self.m = m
+        self.t = t 
 
-eat = [list(map(int, input().split())) for _ in range(d)]  # [사람 번호, 치즈 번호, 시간]
-sick = [list(map(int, input().split())) for _ in range(s)]  # [사람 번호, 아픈 시간]
 
-spoil_chz = set()
+class Info_2:
+    def __init__(self, p,t):
+        self.p = p 
+        self.t = t 
 
-# t초까지 반복하면서, 치즈를 배열에 기록함
-for t in range(1, 101):
-    # t시간에 아팠다면, 그 사람이 t시간 전까지 어떤 치즈를 먹었는지 기록
-    for i in range(s):
-        if sick[i][1] == t:
-            for j in range(51):
-                if arr[sick[i][0]][j] == 1:
-                    spoil_chz.add(j)  # 해당 치즈는 상했다고 가정
+inf_1 = []
+for i in range(d):
+    p,m,t = tuple(map(int, input().split()))
+    inf_1.append(Info_1(p,m,t))
 
-    # 누가 치즈를 먹었는지 기록
-    for i in range(d):
-        if eat[i][2] == t:
-            arr[eat[i][0]][eat[i][1]] = 1
+inf_2 = []
+for i in range(s):
+    p,t = tuple(map(int, input().split()))
+    inf_2.append(Info_2(p,t))
 
-# 최종적으로 오염된 치즈를 먹은 사람 수 계산
-ans = 0
+ans = 0 
 
-for i in range(51):
-    for j in spoil_chz:
-        if arr[i][j] == 1:
-            ans += 1
-            break  # 하나라도 감염된 치즈를 먹었으면 더 확인할 필요 없음
+for i in range(1, m+1):
+    time = [0] * (n+1)
+
+    for info_1 in inf_1:
+        if info_1.m != i :
+            continue
+        
+        p = info_1.p
+
+        if time[p] == 0 :
+            time[p] = info_1.t
+        elif time[p] > info_1.t:
+            time[p] = info_1.t
+        
+    
+    possible = True 
+
+    for info_2 in inf_2:
+        person = info_2.p
+
+        if time[person] == 0 :
+            possible = False
+        
+        if time[person] >= info_2.t:
+            possible = False
+    
+    pill = 0 
+
+    if possible :
+        for i in range(1, n+1):
+            if time[i] != 0 :
+                pill += 1
+        
+        ans = max(ans, pill)
+
 
 print(ans)
