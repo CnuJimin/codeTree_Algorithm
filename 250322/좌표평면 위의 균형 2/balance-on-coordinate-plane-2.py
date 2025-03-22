cac7bb05a82e292a7bbd048b29f35260e3b150c0
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-22 16:13:28 +0900

    Title: 좌표평면 위의 균형 2, Time: 73ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250322/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -0,0 +1,30 @@
+import sys
+
+n = int(input())
+
+points = [tuple(map(int, input().split())) for _ in range(n)]
+
+ans = sys.maxsize
+
+#두개의 선을 선택함 
+for x_line in range(0, 101, 2):
+    for y_line in range(0, 101, 2):
+        a,b,c,d = 0, 0, 0, 0
+
+        for x,y in points:
+            if x > x_line and y > y_line:
+                a += 1
+            if x < x_line and y > y_line:
+                b += 1
+            if x < x_line and y < y_line:
+                c += 1
+            if x > x_line and y < y_line:
+                d += 1
+        
+        print(f"x축 : {x_line}, y축: {y_line} => A: {a}, B: {b}, C: {c}, D: {d}")
+        m = max(a,b,c,d)
+    
+    ans = min(m, ans)
+
+print(ans)
+
