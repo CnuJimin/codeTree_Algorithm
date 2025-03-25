df84d3acec504bc1a3e896752a87cf4699acc18c
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-25 11:05:34 +0900

    Title: 독서실의 거리두기 4, Time: 74ms, Memory: 18MB, Status: Passed - CodeTree

--- 250325/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 250325/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,27 +1,36 @@
+import sys
+
 n = int(input())
 arr = list(map(int,list(input())))
 
 ans = 0 
+# print(*arr)
 #0인 곳에 일단 좌석을 배치하고, 거리간의 차이를 구함 
 for i in range(n):
     if arr[i] == 0 :
         arr[i] = 1 
+
         for j in range(i+1, n):
             if arr[j] == 0 :
                 arr[j] = 1
-            
-            #거리 구하기 
-            for k in range(n):
-                for l in range(k+1, n):
-                    if arr[k] == 1 and arr[l] == 1 :
-                        dist = l - k 
-                    
-                    ans = max(ans, dist)
-                    break
+                result  = sys.maxsize
 
+                #거리 구하기 
+                # print(* arr)
+                for k in range(n):
+                    dist = 0 
+                    for l in range(k+1, n):
+                        if arr[k] == 1 and arr[l] == 1 :
+                            # print(k, l)
+                            dist = l - k 
+                        
+                            result = min(result, dist)
+                            # print(result)
+                            # print(dist)
+                            break
 
-            arr[j] = 0 
-    
-    arr[i] = 0 
+                ans = max(ans, result)
+                arr[j] = 0 
+        arr[i] = 0 
 
 print(ans)
