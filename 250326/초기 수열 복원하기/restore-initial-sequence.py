0675df2e49a1c248c2df26e98b922ac98f736621
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-26 15:01:12 +0900

    Title: 초기 수열 복원하기, Time: 3040ms, Memory: 20MB, Status: Passed - CodeTree

--- 250326/초기 수열 복원하기/restore-initial-sequence.py
+++ 250326/초기 수열 복원하기/restore-initial-sequence.py
@@ -4,9 +4,12 @@ visited = [0] * (n+1)
 result = []
 #1~n까지 수 중에 2개를 골라서 합이 되는 경우를 n-1번 확인 
 
+
 #처음 들어갈 수를 일단 고름
 for i in range(1,n+1):
     result = []
+    visited = [0] * (n+1)
+
     result.append(i)
     visited[i] = 1
     flag = True
@@ -19,6 +22,7 @@ for i in range(1,n+1):
                     result.append(k)
 
                     # print("몇번째 arr = ", j, "값 추가, ", *result)
+                    # print(*visited)
                     visited[k] = 1
                     flag = False
                     if len(result) == n :
@@ -26,7 +30,6 @@ for i in range(1,n+1):
                     break
         
         if flag:
-            visited = [0] * (n+1)
             break
 
 
