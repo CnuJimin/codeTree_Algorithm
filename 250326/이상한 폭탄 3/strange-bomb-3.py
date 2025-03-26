455d260887b42e5913c5faeb376af47c249bdf06
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-26 21:24:12 +0900

    Title: 이상한 폭탄 3, Time: 51ms, Memory: 24MB, Status: Wrong Answer - Codetree

--- 250326/이상한 폭탄 3/strange-bomb-3.py
+++ 250326/이상한 폭탄 3/strange-bomb-3.py
@@ -3,18 +3,22 @@ n, k = map(int, input().split())
 bombs = [int(input()) for _ in range(n)]
 
 bomb = [0] * 1000001
-# 
+visited = [0] * n 
 
 # print(*bombs)
 for i in range(n-k):
     for j in range(i+1,i+k+1):
         # print(i,j)
         if bombs[i] == bombs[j] :
+            if visited[i] == 0 :
+                visited[i] = 1
+                bomb[bombs[i]] += 1
+            if visited[j] == 0:
+                visited[j] = 1
+                bomb[bombs[j]] += 1
             # print(i,j)
-            bomb[bombs[i]] += 1
-            # break
 
-# print(*bomb[0:10])
+# print(*bomb[0:25])
 
 max_value = max(bomb)
 
@@ -24,7 +28,7 @@ for i in range(n):
     if bomb[i] == max_value:
         result.append(i)
 
-# print(*result)
+print(*result)
 
 if max_value == 0 :
     print(0)
