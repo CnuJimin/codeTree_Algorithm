b71b9b9efec4c0489a4fa5fb2c419610e7318b53
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-27 13:13:24 +0900

    Title: 언덕 깎기, Time: 83ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250327/언덕 깎기/hill-cutting.py
+++ 250327/언덕 깎기/hill-cutting.py
@@ -1,26 +1,26 @@
+import sys
 #최저 높이와 최고 높이를 찾고
 #정렬을 하고, 차례대로 맨 앞과 맨 뒤 를 한번씩 올리고 내리는 것을 반복하는건 어떤가??
 
 n = int(input())
 
 arr = [int(input()) for _ in range(n)]
-low_cost = 0 
-high_cost = 0 
-while True:
-    arr.sort()
-    #가장 낮은 언덕과 가장 높은 언덕의 높이 차가 17이하면 멈춤 
-    if arr[-1] - arr[0] <= 17:
-        break
+
+ans = sys.maxsize
+
+
+for i in range(101):
+    cost = 0 
+
+    for j in range(n):
+
+        if arr[j] > i + k:
+            cost += (arr[j] - i - k) * (arr[j] - i - k)
+        
+        elif arr[j] < i :
+            cost += (i - arr[j]) * (i - arr[j])
+        
     
-    arr[0] += 1 
-    low_cost += 1
-    if arr[-1] - arr[0] <= 17:
-        break
+    ans = min(ans, cost)
 
-
-    arr[-1] -= 1
-    high_cost += 1
-    if arr[-1] - arr[0] <= 17:
-        break
-
-print(low_cost * low_cost + high_cost *high_cost)
+print(ans)
