aa09dd3706c8c7f4aac74d7d1a0826f52ea71e71
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-03-31 18:42:31 +0900

    Title: 독서실의 거리두기 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250331/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,24 @@
+n = int(input())
+
+arr = list(map(int, list(input())))
+
+#좌석 간의 거리를 배열에 넣어두고, 그 최댓값이 배열에 몇개 있는지 찾아보기 
+
+dist = []
+
+for i in range(n):
+    for j in range(i+1, n):
+        if arr[i] == 1 and arr[j] == 1 :
+            dist.append(j-i)
+            break
+
+# print(*dist)
+
+value = max(dist)
+
+ans = 0
+for i in dist:
+    if i == value:
+        ans+= 1
+
+print(ans)
