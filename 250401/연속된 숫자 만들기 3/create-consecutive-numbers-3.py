eec0a1404ddc05e6b48c43004ebbdd07c41ef36f
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-04-01 14:11:47 +0900

    Title: 연속된 숫자 만들기 3, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250401/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 250401/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -1,6 +1,6 @@
 #바로 옆으로 이동하면 됨 
 arr = list(map(int ,input().split()))
-arr.sort
+arr.sort()
 
 #처음부터 1열로 서 있을 때 -> 0
 if arr[1] - arr[0] == 1 and arr[2] - arr[1] == 1:
@@ -10,4 +10,4 @@ elif arr[1] - arr[0] == 2 or arr[2] - arr[1] == 2 :
     print(1)
 else:
     print(max(arr[1] - arr[0], arr[2] - arr[1]) - 1)
-#12 7 -> 267 -> 236 -> 346 -> 456 3456 아닌 경우는 차이 중 큰 수의 n - 1
+#12 7 -> 267 -> 236 -> 346 -> 456 3456 아닌 경우는 차이 중 큰 수의 n - 1a
