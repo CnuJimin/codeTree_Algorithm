890be3e002eb96c7e3517bc42da5e2ef80045dc1
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-04-01 14:13:34 +0900

    Title: 연속된 숫자 만들기 3, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250401/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 250401/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -6,7 +6,7 @@ arr.sort()
 if arr[1] - arr[0] == 1 and arr[2] - arr[1] == 1:
     print(0) 
 #둘 중 하나가 2차이 날 때 -> 1
-elif arr[1] - arr[0] == 2 or arr[2] - arr[1] == 2 :
+elif (arr[1] - arr[0] == 2 and arr[2] - arr[1] == 1) or (arr[1] - arr[0] == 1 and arr[2] - arr[1] == 2) :
     print(1)
 else:
     print(max(arr[1] - arr[0], arr[2] - arr[1]) - 1)
