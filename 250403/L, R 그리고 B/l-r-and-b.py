8555513fc358a7691a562b5e9a55bfe98091fcf8
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-04-03 10:18:55 +0900

    Title: L, R 그리고 B, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250403/L, R 그리고 B/l-r-and-b.py
+++ 250403/L, R 그리고 B/l-r-and-b.py
@@ -17,9 +17,15 @@ for i in range(10):
 # print(x1,y1)
 # print(x2,y2)
 
-if x3 == x1 == x2 :
-    print(1 + abs(y2-y1))
+if x3 == x1 == x2:
+    if y1<y3<y2 or y1>y3>y2:
+        print(1 + abs(y2-y1))
+    if max(y1,y2)<y3 or min(y1,y2)>y3:
+        print(abs(y2-y1)-1)
 elif y1== y2 == y3:
-    print(1 + abs(x2-x1))
+    if x1<x3<x2 or x1>x3>x2:
+        print(1 + abs(x2-x1))
+    if max(x1,x2)<x3 or min(x1,x2)>x3:
+        print(abs(x2-x1)-1)
 else:
     print(abs(x2-x1) + abs(y2-y1) - 1)
