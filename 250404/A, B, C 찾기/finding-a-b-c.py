1288c674f6f9d1627b316ba3d4625682bac4f954
CnuJimin <138632648+CnuJimin@users.noreply.github.com>
2025-04-04 10:39:58 +0900

    Title: A, B, C 찾기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/A, B, C 찾기/finding-a-b-c.py
+++ 250404/A, B, C 찾기/finding-a-b-c.py
@@ -2,19 +2,11 @@ arr = list(map(int, input().split()))
 
 arr.sort()
 
-val = max(arr)
+a = arr[0] 
+b = arr[1]
+if arr[2] == a+b:
+    c = arr[3]
+else:
+    c = arr[2]
 
-for a in range(1, val):
-    for b in range(a, val):
-        for c in range(b,val):
-            result = []
-            result.append(a)
-            result.append(b)
-            result.append(c)
-            result.append(a+b)
-            result.append(a+c)
-            result.append(b+c)
-            result.append(a+b+c)
-            result.sort()
-            if result == arr:
-                print(a,b,c)
+print(a,b,c)
