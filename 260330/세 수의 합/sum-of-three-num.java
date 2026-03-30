22204f63fad4aeaabc2fefe807cd6e8bb3fb04a8
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-30 17:01:02 +0900

    Title: 세 수의 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260330/세 수의 합/sum-of-three-num.java
+++ 260330/세 수의 합/sum-of-three-num.java
@@ -7,7 +7,7 @@ public class Main {
     static long K;
     static long[] nums;
     static int[] selected;
-    static HashMap<Long, List<long[]>> map = new HashMap<>();
+    static HashMap<Long, List<int[]>> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,41 +20,39 @@ public class Main {
 
         nums = new long[N];
         selected = new int[2];
-        for (int i = 0 ; i < N ; i++){
+
+        for (int i = 0; i < N; i++) {
             nums[i] = Long.parseLong(st.nextToken());
         }
 
-        //2개 골라 
         comb(0, 0);
-        int cnt = 0;
-        for (int i = 0 ; i < N ; i ++){
+
+        long cnt = 0;
+        for (int i = 0; i < N; i++) {
             long findNum = K - nums[i];
-            if(map.containsKey(findNum)){
-                for(long[] arr : map.get(findNum)){
-                    if(arr[0] != i && arr[1] != i){
-                        cnt ++;
+
+            if (map.containsKey(findNum)) {
+                for (int[] arr : map.get(findNum)) {
+                    if (arr[0] != i && arr[1] != i) {
+                        cnt++;
                     }
                 }
             }
         }
 
-        System.out.println(cnt);
-
+        System.out.println(cnt / 3);
     }
 
-    static void comb(int idx, int cnt){
-        if(cnt == 2){
-            // System.out.println(Arrays.toString(selected));
-            long sum = 0 ;
-            for (long i : selected){
-                sum += i;
-            }
-            map.put(sum, new ArrayList<>());
-            map.get(sum).add(new long[]{selected[0], selected[1]});
+    static void comb(int idx, int cnt) {
+        if (cnt == 2) {
+            long sum = nums[selected[0]] + nums[selected[1]];
+
+            map.putIfAbsent(sum, new ArrayList<>());
+            map.get(sum).add(new int[]{selected[0], selected[1]});
             return;
         }
 
-        for (int i = idx ; i < N ; i ++){
+        for (int i = idx; i < N; i++) {
             selected[cnt] = i;
             comb(i + 1, cnt + 1);
         }
