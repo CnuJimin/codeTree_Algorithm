526a3bbdce39dd8f17aa4911f3104714e3831a04
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-03-30 11:07:46 +0900

    Title: 가장 많은 데이터, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260330/가장 많은 데이터/most-frequent-data.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int max = -1;
+
+        HashMap<String, Integer> map = new HashMap<>();
+        while (N -- > 0){
+            String word = br.readLine();
+
+            if(map.containsKey(word)){
+                map.put(word, map.get(word) + 1);
+                max = Math.max(max, map.get(word));
+            }else{
+                map.put(word, 1);
+            }
+            
+        }
+
+        System.out.println(max);
+
+    }
+}
