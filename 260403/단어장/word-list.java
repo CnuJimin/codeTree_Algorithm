98e3b79b8441e41f044c087cc08c72635b928cb3
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-04-03 12:19:25 +0900

    Title: 단어장, Time: 0ms, Memory: 0MB, Status: Compile Error - Codetree

---
+++ 260403/단어장/word-list.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+import java.util.Map.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        TreeMap<String, Integer> map = new TreeMap<>();
+
+        
+
+        while(N -- > 0){
+            String word = br.readLine();
+
+            if (!map.containsKey(word)) map.put(word, 1);
+            else map.put(word, map.get(word) + 1);
+        }
+
+        Iterator<Entry<String, Integer>> it = map.entrySet().iterator();
+
+        while(it.hasNext()){
+            Entey<String, Integer> entry = it.next();
+
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
