7894cc59ac48a9d9ca0b7aff0a24ce277fa7efcb
jimin Choi <138632648+ji-mim@users.noreply.github.com>
2026-05-12 17:45:09 +0900

    [Passed] top K 숫자 (959ms, 36MB)

---
+++ 260512/top K 숫자/top-k-elements.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        TreeSet<Integer> ts = new TreeSet<>();
+        for(int i = 0 ; i < N ; i ++){
+            ts.add(sc.nextInt());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0 ; i < K ; i ++){
+            sb.append(ts.last() + " ");
+            ts.remove(ts.last());
+        }
+
+        System.out.println(sb);
+
+    }
+}
